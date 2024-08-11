b7f1f9bcca324db636ff35a2be50f1e3fd6d92fc
osmannyildiz <iamosmannyildiz@gmail.com>
2024-08-11 19:45:26 +0300

    finish vault

--- 06-vault/anchor/programs/vault/src/lib.rs
+++ 06-vault/anchor/programs/vault/src/lib.rs
@@ -25,13 +25,44 @@ pub mod vault {
         transfer(cpi_ctx, amount)
     }
 
-    // pub fn withdraw(ctx: Context<DepositOrWithdrawAccs>) -> Result<()> {
-    //     Ok(())
-    // }
+    pub fn withdraw(ctx: Context<DepositOrWithdrawAccs>, amount: u64) -> Result<()> {
+        let cpi_program = ctx.accounts.system_program.to_account_info();
+        let cpi_accounts = Transfer {
+            from: ctx.accounts.vault.to_account_info(),
+            to: ctx.accounts.user.to_account_info(),
+        };
 
-    // pub fn close(ctx: Context<CloseAccs>) -> Result<()> {
-    //     Ok(())
-    // }
+        let vault_seeds = &[
+            b"vault",
+            ctx.accounts.vault_state.to_account_info().key.as_ref(),
+            &[ctx.accounts.vault_state.vault_bump],
+        ];
+        let signer_seeds = &[&vault_seeds[..]];
+
+        let cpi_ctx = CpiContext::new_with_signer(cpi_program, cpi_accounts, signer_seeds);
+
+        transfer(cpi_ctx, amount)
+    }
+
+    pub fn close(ctx: Context<CloseAccs>) -> Result<()> {
+        let cpi_program = ctx.accounts.system_program.to_account_info();
+        let cpi_accounts = Transfer {
+            from: ctx.accounts.vault.to_account_info(),
+            to: ctx.accounts.user.to_account_info(),
+        };
+
+        let vault_seeds = &[
+            b"vault",
+            ctx.accounts.vault_state.to_account_info().key.as_ref(),
+            &[ctx.accounts.vault_state.vault_bump],
+        ];
+        let signer_seeds = &[&vault_seeds[..]];
+
+        let cpi_ctx = CpiContext::new_with_signer(cpi_program, cpi_accounts, signer_seeds);
+
+        let amount = ctx.accounts.vault.lamports();
+        transfer(cpi_ctx, amount)
+    }
 }
 
 #[derive(Accounts)]
@@ -41,12 +72,21 @@ pub struct InitializeAccs<'info> {
     pub user: Signer<'info>,
 
     // The PDA that will store the state (currently just bumps) for this particular user
-    #[account(init, payer = user, seeds = [b"vault_state", user.key().as_ref()], bump, space = VaultState::INIT_SPACE)]
+    #[account(
+        init,
+        payer = user,
+        seeds = [b"vault_state", user.key().as_ref()],
+        bump,
+        space = VaultState::INIT_SPACE
+    )]
     pub vault_state: Account<'info, VaultState>,
 
     // The PDA that will store the lamports for this particular user
     // Even though it doesn't exist yet, we don't init it. Because it can be initted when the user deposits into the vault for the first time
-    #[account(seeds = [b"vault", vault_state.key().as_ref()], bump)]
+    #[account(
+        seeds = [b"vault", vault_state.key().as_ref()],
+        bump
+    )]
     pub vault: SystemAccount<'info>,
 
     // Needed for creating accounts and transferring lamports
@@ -58,10 +98,40 @@ pub struct DepositOrWithdrawAccs<'info> {
     #[account(mut)]
     pub user: Signer<'info>,
 
-    #[account(seeds = [b"vault_state", user.key().as_ref()], bump)]
+    #[account(
+        seeds = [b"vault_state", user.key().as_ref()],
+        bump = vault_state.vault_state_bump
+    )]
     pub vault_state: Account<'info, VaultState>,
 
-    #[account(seeds = [b"vault", vault_state.key().as_ref()], bump)]
+    #[account(
+        mut,
+        seeds = [b"vault", vault_state.key().as_ref()],
+        bump = vault_state.vault_bump
+    )]
+    pub vault: SystemAccount<'info>,
+
+    pub system_program: Program<'info, System>,
+}
+
+#[derive(Accounts)]
+pub struct CloseAccs<'info> {
+    #[account(mut)]
+    pub user: Signer<'info>,
+
+    #[account(
+        mut,
+        close = user,
+        seeds = [b"vault_state", user.key().as_ref()],
+        bump = vault_state.vault_state_bump
+    )]
+    pub vault_state: Account<'info, VaultState>,
+
+    #[account(
+        mut,
+        seeds = [b"vault", vault_state.key().as_ref()],
+        bump = vault_state.vault_bump
+    )]
     pub vault: SystemAccount<'info>,
 
     pub system_program: Program<'info, System>,
