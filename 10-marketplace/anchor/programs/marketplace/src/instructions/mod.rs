b399494e9829ae740422be38022860c8fdb25ffd
osmannyildiz <iamosmannyildiz@gmail.com>
2024-08-29 16:17:02 +0300

    marketplace: implement `init`, `list` and `delist`

--- 10-marketplace/anchor/programs/marketplace/src/instructions/mod.rs
+++ 10-marketplace/anchor/programs/marketplace/src/instructions/mod.rs
@@ -1,3 +1,9 @@
-pub mod initialize;
+pub mod delist;
+pub mod init;
+pub mod list;
+pub mod purchase;
 
-pub use initialize::*;
+pub use delist::*;
+pub use init::*;
+pub use list::*;
+pub use purchase::*;
--- 10-marketplace/anchor/programs/marketplace/src/lib.rs
+++ 10-marketplace/anchor/programs/marketplace/src/lib.rs
@@ -15,7 +15,19 @@ declare_id!("67pguSETAjZra9HoX9g7CvLdFYtn9U8CHZhxUrRFs4dj");
 pub mod marketplace {
     use super::*;
 
-    pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
-        initialize::handler(ctx)
+    pub fn init(ctx: Context<Init>, name: String, fee: u16) -> Result<()> {
+        ctx.accounts.init(name, fee, &ctx.bumps)
     }
+
+    pub fn list(ctx: Context<List>, price: u64) -> Result<()> {
+        ctx.accounts.create_listing(price, &ctx.bumps)?;
+        ctx.accounts.deposit_nft()
+    }
+
+    pub fn delist(ctx: Context<Delist>) -> Result<()> {
+        ctx.accounts.withdraw_nft()
+    }
+
+    // TODO Implement purchase
+    // pub fn purchase(ctx: Context<Purchase>) -> Result<()> {}
 }
