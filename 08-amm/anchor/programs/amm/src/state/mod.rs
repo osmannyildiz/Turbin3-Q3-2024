2f97df7946652607891d99d1959de7108da279cd
osmannyildiz <iamosmannyildiz@gmail.com>
2024-09-10 09:57:31 +0300

    amm: move structs into individual files

--- 08-amm/anchor/programs/amm/src/state/mod.rs
+++ 08-amm/anchor/programs/amm/src/state/mod.rs
@@ -1,13 +1,3 @@
-use anchor_lang::prelude::*;
+pub mod config;
 
-#[account]
-#[derive(InitSpace)]
-pub struct AmmConfig {
-    pub init_state: u8,
-    pub seed: u64,
-    pub fee: u16, // 0% => 0, 100% => 10000
-    pub mint_x: Pubkey,
-    pub mint_y: Pubkey,
-    pub mint_lp_bump: u8,
-    pub bump: u8,
-}
+pub use config::*;
